5c20a3bbc1debc516e7cbe51ecc523c0b66b77a4
Jmukumzoda <jmukumzoda63@gmail.com>
2023-06-14 19:08:43 +0500

    first

---
+++ js/main.js
@@ -0,0 +1,22 @@
+let obj = {
+    school: "shool2",
+    school1: "shool1",
+    school2: "shool29",
+}
+let obj2 = {
+    name:"Harry",
+    name2: "Maks",
+    name3:"Katy"
+}
+
+let obj_vmeste = Object.assign(obj,obj2);
+let arr = Object.values(obj_vmeste)
+console.log(arr);
+
+let a_rr = ['Jon','Bob','Harry','Maks','Tima']
+
+let name_prompt = prompt('Ведите имя ')
+
+alert('Ответ в консоли')
+console.log(a_rr);
+    
